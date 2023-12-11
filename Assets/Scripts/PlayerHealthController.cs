a00b0295ebdd5968c1203603ca8e361433a8757d
Abhishek Kadam <kadamabhishek981111@gmail.com>
2023-12-11 21:15:29 +0530

    Created Lobby scene with UI and game over UI

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -6,12 +6,32 @@ public class PlayerHealthController : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
     public Slider healthSlider;
+    // public Transform player;
+    // public Transform healthSliderTransform;
+    // private Quaternion initialRotation;
 
     void Start()
     {
         currentHealth = maxHealth;
         UpdateUI(); 
+
+        // if (healthSliderTransform != null)
+        // {
+        //     initialRotation = healthSliderTransform.rotation;
+        // }
     }
+
+    // void Update()
+    // {
+    //     if (player != null && healthSliderTransform != null)
+    //     {
+    //         Vector3 abovePlayerHead = player.position + Vector3.up * 1; //*Distance above head to multiply
+    //         healthSliderTransform.position = abovePlayerHead;
+
+    //         //initial rotation to maintain consistent orientation
+    //         healthSliderTransform.rotation = initialRotation;
+    //     }
+    // }
 
     public void TakeDamage(int damage)
     {
